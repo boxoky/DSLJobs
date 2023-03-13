319bb37e0155c692c03a16b9ff75fad2f4b2016b
Marco Antonio AG <marcontonio.98@hotmail.com>
2023-03-12 20:21:03 -0600

    Se agrega el script sh

---
+++ jobscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Bienvenido humano a Planet Dreck444"
+
+if [ "$agente" = "true" ]
+then
+	echo "Agente X haz sido elejido apra ir al planeta $planeta"
+else
+    echo "Ciudadano $nombre sera entrenado para convertirse en agente espacial"
+fi
+
+sleep 5
+echo "Sera una mision suicida, pero eres el unico que puede lograrlo."
